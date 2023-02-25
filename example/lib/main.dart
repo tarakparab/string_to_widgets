49d1bac73607488d43b1b797dff17259b49e988e
Tarak Parab <tarakparab@protonmail.com>
2023-02-25 17:44:27 +0530

    Initial commit 3

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_math_fork/flutter_math.dart';
+import 'package:string_to_widgets.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,13 +31,10 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final text =
+      'Equation for area of square is given by: /startEquation area = length^2 /endEquation in sq.m and /startEquation perimeter = length*4 /endEquation in metre where, /startSymbol length = side of the square. /endSymbol';
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  final notations = ['Equation', 'Symbol'];
 
   @override
   Widget build(BuildContext context) {
@@ -46,22 +45,52 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: stringToWidgets(
+              text: text,
+              notations: notations,
+              context: context,
+              switchStatement: _switchStatement),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
     );
+  }
+
+  Widget _switchStatement(
+      int widgetIndex, String string, BuildContext context) {
+    switch (widgetIndex) {
+      case 0:
+        return Center(
+          child: Math.tex(
+            string,
+            textStyle: Theme.of(context)
+                .textTheme
+                .titleMedium!
+                .copyWith(fontWeight: FontWeight.bold),
+          ),
+        );
+
+      case 1:
+        var equation = string.split('=');
+
+        return Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Math.tex(
+              equation[0],
+              textStyle: Theme.of(context)
+                  .textTheme
+                  .titleMedium!
+                  .copyWith(fontWeight: FontWeight.bold),
+            ),
+            Text(
+              ' = ${equation[1]}',
+            ),
+          ],
+        );
+
+      default:
+        return Text(string);
+    }
   }
 }
